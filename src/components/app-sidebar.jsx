d25e6967920a20d0ecb19445dc4ee48c53bd51b4
MRTitus02 <titus.senthilkumaran@teceze.com>
2026-04-20 18:10:28 +0530

    project init && dashboard

---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,53 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import { File, Pencil, Settings } from "lucide-react"
+import Link from "next/link";
+
+
+const MenuItems = [
+  {
+    label: "Dashboard",
+    icon: File,
+    url: `/dashboard`,
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    url: `/settings`,
+  },
+  {
+    label: "Profile",
+    icon: Pencil,
+    url: `/profile`,
+  },
+  {
+    label: "Help",
+    icon: File,
+    url: `/help`,
+  }
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+          {MenuItems.map(each => {
+            return(
+              <Link href={each.url} className="flex items-center gap-2">
+                <each.icon></each.icon>{each.label}
+              </Link>
+            )
+          })}
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
